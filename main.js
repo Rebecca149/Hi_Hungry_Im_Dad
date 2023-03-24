adcf24e0317293aebf66f485f76cde53b3db7c98
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2023-03-24 13:45:12 +0000

    added getAnotherJoke to getJoke function

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ const chat = document.querySelector('.chat')
 const userName = document.querySelector('#user-name');
 userName.addEventListener('keypress', getName)
 
-
+// gets user's name, asks question and displays input field
 function getName(event) {
     if (event.key === "Enter") {
     const greeting = document.querySelector(".greeting");
@@ -47,12 +47,15 @@ function getName(event) {
     
 }
 
+// creates an input field, appends it to chat div, and adds an event listener to it 
 function createInputField(){
     const input = document.createElement('input')
     chat.appendChild(input)
     input.addEventListener('keypress', getJoke)
 }
 
+
+// calls getFetch to get joke, creates a p element, appends it to chat div and then displays the joke in the div
 async function getJoke(event){
     if (event.key === "Enter") {
     const joke = await getFetch()
@@ -60,10 +63,12 @@ async function getJoke(event){
     const jokeDisplay = document.createElement('p')
     chat.appendChild(jokeDisplay)
     jokeDisplay.textContent = joke 
+    getAnotherJoke() //add setTimeout();
     }
 
 }
 
+//get data from dad joke api
 async function getFetch() {
     let response = await fetch("https://icanhazdadjoke.com/", {
         headers: { accept: "application/json" },
@@ -73,13 +78,20 @@ async function getFetch() {
     return data.joke
 }
 
+
+// creates new p element, appends it to div, asks question and calls creatInputField()
 function getAnotherJoke () {
     const newP = document.createElement('p')
     chat.appendChild(newP)
     newP.textContent = 'Do you want to hear another joke?'
     createInputField()
+    getLastInput()
 }
 
+function getLastInput(){
+    let inputList = document.querySelectorAll(".chat input")
+    console.log(Array.from(inputList))
+}
 
 
 
