240166aeb069a72b4c8bd8e21b944af734ff187d
shamalamm <shamy_pw4@hotmail.co.uk>
2023-03-24 10:09:04 +0000

    plan created

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
+/*
+                  Plan
+- Create website to fetch dad jokes from api. 
 
+- Dad will ask for name. 
+    - Text will display in <p></p>
+
+- User will be able to input name using an input field.
+    - Create input field in html
+    - Link html input with JS event listener.
+    - Save input in a variable using (event.target.value)
+
+- Dad will greet user by name & ask if you would like to hear a joke.
+    - Use above variable as part of greeting string (Use ${})
+
+- User will have an option to input Yes/No.
+    - Dad tells joke regardless of answer
+    - Special keyword to stop jokes.
+
+- Dad will tell joke anyway.
+    - Dads like to tell jokes.
+
+Bonus tasks 
+
+- Bdum tss sound plays after joke.
+- Would you like to hear another one?
+- increase joke intensity
+
+
+*/
 async function getFetch() {
-    let response = await fetch("https://pokeapi.co/api/v2/pokemon/1/")
+    let response = await fetch("https://icanhazdadjoke.com/", {
+        headers: { accept: "application/json" },
+      });
     let data = await response.json()
-    console.log(data)
+    console.log(data.joke)
     return response
 }
 
